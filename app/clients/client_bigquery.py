153e9480c5ab0aec1d246ae56ed9d460332d3ae9
Lucas Batista <lucaskvy@gmail.com>
2026-06-26 19:28:16 -0300

    update

--- app/clients/client_bigquery.py
+++ app/clients/client_bigquery.py
@@ -3,3 +3,19 @@
 
 # Aqui vai ter todo o codigo da classe ClientBigQuery, esta classe vai ser responsavel
 # por fazer a conexão com o bigqeury para retornar os chunks em embeddings e texto para recuperação hibrida
+
+
+from google.cloud import bigquery
+
+from app.config import (
+    PROJECT_ID,
+    BQ_DATASET,
+)
+
+class ClientBigQuery:
+
+    def __init__(self):
+
+        self.client = bigquery.Client(
+            project=PROJECT_ID
+        )
