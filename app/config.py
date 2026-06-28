21408bf4e4ad9cd7fd8871065ded11d8b52af065
lucas souza <lucaskvy@gmail.com>
2026-06-28 13:03:24 -0300

    Atualiza dependências do projeto

--- app/config.py
+++ app/config.py
@@ -9,12 +9,12 @@ REGION = os.getenv("REGION")
 IAM_ACCOUNT = os.getenv("IAM_ACCOUNT")
 
 BQ_DATASET = os.getenv("BQ_DATASET")
-BUCKET_NAME = os.getenv("BUCKET_NAME")
 BQ_TABLE = os.getenv("BQ_TABLE")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 MODELO_CHAT = os.getenv("MODELO_CHAT")
 MODELO_EMBEDDINGS = os.getenv("MODELO_EMBEDDINGS")
 
-TOP_K = int(os.getenv("TOP_K"))
-TEMPERATURE = float(os.getenv("TEMPERATURE"))
-MAX_OUTPUT_TOKENS = int(os.getenv("MAX_OUTPUT_TOKENS"))
+TOP_K = int(os.getenv("TOP_K", "8"))
+TEMPERATURE = float(os.getenv("TEMPERATURE", "0.2"))
+MAX_OUTPUT_TOKENS = int(os.getenv("MAX_OUTPUT_TOKENS", "1024"))
